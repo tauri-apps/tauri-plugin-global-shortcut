e7e6553c18517ab74c00e7a97539047d54a272c9
FabianLars <fabianlars@fabianlars.de>
2023-05-03 07:04:31 +0000

    Merge branch 'dev' into next. Rename next to v2
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/4869196953
    
    Co-authored-by: FabianLars <FabianLars@users.noreply.github.com>

---
+++ dist-js/index.mjs
@@ -0,0 +1,98 @@
+import { invoke, transformCallback } from '@tauri-apps/api/tauri';
+
+// Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+/**
+ * Register a global shortcut.
+ * @example
+ * ```typescript
+ * import { register } from 'tauri-plugin-global-shortcut-api';
+ * await register('CommandOrControl+Shift+C', () => {
+ *   console.log('Shortcut triggered');
+ * });
+ * ```
+ *
+ * @param shortcut Shortcut definition, modifiers and key separated by "+" e.g. CmdOrControl+Q
+ * @param handler Shortcut handler callback - takes the triggered shortcut as argument
+ *
+ * @since 1.0.0
+ */
+async function register(shortcut, handler) {
+    return await invoke('plugin:globalShortcut|register', {
+        shortcut,
+        handler: transformCallback(handler)
+    });
+}
+/**
+ * Register a collection of global shortcuts.
+ * @example
+ * ```typescript
+ * import { registerAll } from 'tauri-plugin-global-shortcut-api';
+ * await registerAll(['CommandOrControl+Shift+C', 'Ctrl+Alt+F12'], (shortcut) => {
+ *   console.log(`Shortcut ${shortcut} triggered`);
+ * });
+ * ```
+ *
+ * @param shortcuts Array of shortcut definitions, modifiers and key separated by "+" e.g. CmdOrControl+Q
+ * @param handler Shortcut handler callback - takes the triggered shortcut as argument
+ *
+ * @since 1.0.0
+ */
+async function registerAll(shortcuts, handler) {
+    return await invoke('plugin:globalShortcut|register_all', {
+        shortcuts,
+        handler: transformCallback(handler)
+    });
+}
+/**
+ * Determines whether the given shortcut is registered by this application or not.
+ *
+ * If the shortcut is registered by another application, it will still return `false`.
+ *
+ * @example
+ * ```typescript
+ * import { isRegistered } from 'tauri-plugin-global-shortcut-api';
+ * const isRegistered = await isRegistered('CommandOrControl+P');
+ * ```
+ *
+ * @param shortcut shortcut definition, modifiers and key separated by "+" e.g. CmdOrControl+Q
+ *
+ * @since 1.0.0
+ */
+async function isRegistered(shortcut) {
+    return await invoke('plugin:globalShortcut|is_registered', {
+        shortcut
+    });
+}
+/**
+ * Unregister a global shortcut.
+ * @example
+ * ```typescript
+ * import { unregister } from 'tauri-plugin-global-shortcut-api';
+ * await unregister('CmdOrControl+Space');
+ * ```
+ *
+ * @param shortcut shortcut definition, modifiers and key separated by "+" e.g. CmdOrControl+Q
+ *
+ * @since 1.0.0
+ */
+async function unregister(shortcut) {
+    return await invoke('plugin:globalShortcut|unregister', {
+        shortcut
+    });
+}
+/**
+ * Unregisters all shortcuts registered by the application.
+ * @example
+ * ```typescript
+ * import { unregisterAll } from 'tauri-plugin-global-shortcut-api';
+ * await unregisterAll();
+ * ```
+ *
+ * @since 1.0.0
+ */
+async function unregisterAll() {
+    return await invoke('plugin:globalShortcut|unregister_all');
+}
+
+export { isRegistered, register, registerAll, unregister, unregisterAll };
+//# sourceMappingURL=index.mjs.map
